db8a50bd7c46bc0e0debd0242b6fbff05a91aeee
Muharrem GEDİK <39222515+gdkmuharrem@users.noreply.github.com>
2024-06-27 15:23:43 +0300

    Add files via upload

---
+++ 1)python/10-Tuple.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 27 15:19:29 2024
+
+@author: MUHARREM GEDİK
+"""
+
+#%% Tuple
+
+t = (1,2,3,4,5,6,2,4,5,3)
+
+t[2]
+
+dir(t)
+help(t.count)
+
+t.count(2) #tuple içinde 2'den kaç adet olduğunu hesaplar.
+t.index(5) #5'i içinde ilk gördüğü yerin indexini verir.
